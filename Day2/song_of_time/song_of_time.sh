ea7443df3abe1bfd4a412bf43e1234600a38c0e6
Amelie1603 <97541565+Amelie1603@users.noreply.github.com>
2022-03-18 08:51:18 +0100

    Add files via upload

---
+++ Day2/song_of_time/song_of_time.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+my_day=$(date +%A) #les "word " indique une chaîne de caractères #()
+my_date=$(date +%d/%m/%Y)
+my_hour=$(date +%H:%M)
+echo Today is $my_day $my_date and it is $my_hour.
+
